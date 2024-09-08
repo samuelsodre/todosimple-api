f89eb48dc90e305ee1b2b797e44ce20bce84fe28
Samuel Sodre <samuelthobia10@gmail.com>
2024-09-08 16:43:48 -0300

    Refactor WebConfig

--- src/main/java/com/samuel/todosimple/configs/WebConfig.java
+++ src/main/java/com/samuel/todosimple/configs/WebConfig.java
@@ -3,10 +3,11 @@ package com.samuel.todosimple.configs;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
     }
